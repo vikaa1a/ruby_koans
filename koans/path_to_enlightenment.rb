80fcf4500e66108767b9ca2621b6c2956831d954
vikaa1a <114860679+vikaa1a@users.noreply.github.com>
2024-04-16 08:21:25 +0000

    16042024 classwork

--- koans/path_to_enlightenment.rb
+++ koans/path_to_enlightenment.rb
@@ -16,7 +16,7 @@ in_ruby_version("2", "3") do
   require 'about_keyword_arguments'
 end
 require 'about_constants'
-require 'about_regular_expressions'
+#require 'about_regular_expressions'
 require 'about_control_statements'
 require 'about_triangle_project'
 require 'about_exceptions'
--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,10 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if (a + b) <= c  ||  (a + c) <= b || (b + c) <= a ||  (a <= 0) || (b <= 0) || (c <= 0)
+    raise TriangleError, "invalid triangle: #{a}, #{b}, #{c}"
+  end
+  return (a == b) & (b == c) & (a==c) ? :equilateral : (a == b) || (b == c) || (a == c) ? :isosceles : :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
